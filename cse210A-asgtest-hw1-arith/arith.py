a057560c910208a77aff0f80248c09e243fe0081
sukeswan <suryakeswani@gmail.com>
2021-01-13 20:33:07 -0800

    working on Parser

--- cse210A-asgtest-hw1-arith/arith.py
+++ cse210A-asgtest-hw1-arith/arith.py
@@ -24,7 +24,7 @@ class Lexer(object):
         # text, index to iterate through text, and current for current character
         self.text = text
         self.index = 0 
-        self.current = self.text[self.index]
+        self.current = text[self.index]
     
     # move the index up to the next char
     def advance(self):
@@ -57,6 +57,50 @@ class Lexer(object):
             elif self.current == "-":
                 self.advance()
                 return Token(SUB,'-')
+
+# wrapper AST class
+class AST(object):
+    pass
+
+# for math operations with a left int and right int
+class MathOp(AST):
+    def __init__(self,left,right,op):
+        self.left= left
+        self.right = right
+        self.op = op
+        self.token = op 
+
+# for integer values
+class Num(AST):
+    def __init__(self,token):
+        #set value to be the value in the integer token object
+        self.token = token
+        self.value = token.value
+
+class Parser(object):
+    def __init__(self,lexer):
+        self.lexer = lexer
+        self.curr_token = lexer.get_next_token()
+
+    #function to deal with integer types 
+    def numbers(self):
+        token = self.curr_token
+        if token.type == INT:
+            return Num(token)
+        # deal with negative numbers by geting next token and make negative
+        elif token.type == SUB:
+            self.curr_token = self.lexer.get_next_token()
+            token.value = -1 * token.value
+            return Num(token)
+    
+    def multiply(self):
+        
+
+
+
+
+
+
     
 
 
